31edc0785e8748589c4716dfc6137ad62dd4afbe
AnimeshKC <animesh@ualberta.ca>
2020-02-18 15:24:25 -0700

    Update temperatureSensorTesting.ino

--- BMS Project/temperatureSensorTesting/temperatureSensorTesting.ino
+++ BMS Project/temperatureSensorTesting/temperatureSensorTesting.ino
@@ -3,7 +3,7 @@
 #include <OneWire.h>
 
 // Data wire is plugged into pin 2 on the Arduino 
-#define ONE_WIRE_BUS 2 
+#define ONE_WIRE_BUS 2
 /********************************************************************/
 // Setup a oneWire instance to communicate with any OneWire devices  
 // (not just Maxim/Dallas temperature ICs) 
